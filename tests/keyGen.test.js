7788417a5d0889cf8a2ebd7c0ca2e3e7a75e7949
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-29 22:55:28 -0600

    Update keyGen.test.js

--- tests/keyGen.test.js
+++ tests/keyGen.test.js
@@ -1,14 +1,15 @@
 const assert = require('assert');
 const { execSync } = require('child_process');
 const fs = require('fs');
+const path = require('path'); // Add this line
 
 describe('Key Generation', () => {
   it('should generate private and public keys', () => {
     // Run key generation script
-    execSync('node keyGen.js');
+    execSync('node ../keyGen.js'); // Adjust the path to keyGen.js
 
     // Check if files are created
-    assert.ok(fs.existsSync('private-key.pem'));
-    assert.ok(fs.existsSync('public-key.pem'));
+    assert.ok(fs.existsSync(path.resolve(__dirname, '..', 'private-key.pem')));
+    assert.ok(fs.existsSync(path.resolve(__dirname, '..', 'public-key.pem')));
   });
 });
